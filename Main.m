a6d82a272a5bf6f0291241cf1ab8770e754fe0e3
Brady <bradygmoon@gmail.com>
2017-06-05 09:25:26 -0600

    Added Initial data and plots

--- Main.m
+++ Main.m
@@ -1,6 +1,10 @@
 %script
-
-
+targetInit = [7071; 7071; 225; 7.72];
+ownshipInit = [0; 0; 170; 2.57];
+ownshipState = ownshipInit;
+targetState = targetInit;
+targetStateLog = [targetState];
+ownshipStateLog = [ownshipState];
 
 
 
@@ -14,8 +18,13 @@
 
 
 %Figure
-plot(1,1,'x')
-legend('Vehicle')
+plot(ownshipInit(1),ownshipInit(2),'or', 'MarkerSize', 2)
+hold on
+
+plot(targetInit(1),targetInit(2),'ob', 'MarkerSize', 2)
+legend('Target initial','Ownship initial')
+%plot(ownshipStateLog(1,:),ownshipStateLog(2,:),'o')
+legend('Location','northwest')
 xlim([-4000 8000])
 ylim([-4000 8000])
 hold on
