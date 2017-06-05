ef480721583626ab1075d9108a91a4f19cbffd10
Brady <bradygmoon@gmail.com>
2017-06-05 10:03:35 -0600

    Added target movement and graphing

--- Main.m
+++ Main.m
@@ -22,19 +22,25 @@ ownshipState = [ownshipState(1) + ownshipState(4)*60*cosd(ownshipState(3));
     ownshipState(3);
     ownshipState(4)];
 ownshipStateLog = [ownshipStateLog ownshipState];
-
+targetState = [targetState(1) + targetState(4)*60*cosd(targetState(3));
+    targetState(2) + targetState(4)*60*sind(targetState(3));
+    targetState(3);
+    targetState(4)];
+targetStateLog = [targetStateLog targetState];
 
 
 
 end
 
 %Figure
+plot(targetStateLog(1,:),targetStateLog(2,:),'-b')
+hold on
 plot(ownshipStateLog(1,:),ownshipStateLog(2,:),'-r')
 hold on
 plot(ownshipInit(1),ownshipInit(2),'or', 'MarkerSize', 2)
 hold on
 plot(targetInit(1),targetInit(2),'ob', 'MarkerSize', 2)
-legend('Ownship','Target initial','Ownship initial')
+legend('Target','Ownship','Target initial','Ownship initial')
 %plot(ownshipStateLog(1,:),ownshipStateLog(2,:),'o')
 legend('Location','northwest')
 xlim([-4000 8000])
